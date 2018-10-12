6121e97f95cd7c99fbc8ffa0fcae3b5de7d05b84
dark0ghost <40036029+dark0ghost@users.noreply.github.com>
2018-10-12 13:24:37 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,18 +1,33 @@
 from bs4 import BeautifulSoup
 import requests
-def Proxy ():
-    a=pars()
-    for i in a:
-        a = {'http':i}
-        try:
-         requests.get("https://www.google.com/", proxies= a)
-         return i
-        except:
-            continue
+def Proxy (type):
 
+  if type == "socks5":
+     a=pars()
+     for i in a:
+         a = {'http':i}
+         try:
+           requests.get("https://www.google.com/", proxies= a)
+           return i
+         except:
+            continue
+  elif type == "http":
+      pass
+  elif type == "https":
+      pass
+  else:
+      a = pars()
+      for i in a:
+          a = {'http': i}
+          try:
+              requests.get("https://www.google.com/", proxies=a)
+              return i
+          except:
+              continue
 
 def  pars ():
     url = 'http://www.gatherproxy.com/ru/sockslist'
+    #url = 'http://spys.one/socks/'
     r= requests.get(url)
     lis = [ (i*7)+2 for i  in  range(0,24)]
 
@@ -35,6 +50,5 @@ def  pars ():
           port = port[53:60].replace("')","").replace("<","")
           ip_list.append (f"soks5://{ip}:{port}")
     return ip_list
-
 
 
