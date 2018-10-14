984852582aa8d1bdf4f6dd1dd9c2b14aeb55d94f
dark0ghost <40036029+dark0ghost@users.noreply.github.com>
2018-10-14 21:36:27 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,48 +1,28 @@
 from bs4 import BeautifulSoup
 import requests
+
 def Proxy (type):
 
   if type == "socks":
 
 
     if str( requests.get('http://www.gatherproxy.com/ru/sockslist')) == "<Response [200]>":
-        a=pars()
-        for i in a:
-           a = {'http':i}
-           try:
-             requests.get("https://www.google.ru", proxies= a)
-             return i
-           except:
-              continue
 
-
-
+        return pars()
     if str(requests.get("https://www.socks-proxy.net/")) == "<Response [200]>":
-         
-         a = pars2()
-         for i in a:
-             a = {'http': i}
-             try:
-                 requests.get("https://www.google.ru", proxies=a)
-                 return i
-             except:
-                 Proxy(type)
-
+        return pars2()
   elif type == "http":
       pass
 
 def  pars ():
     url = 'http://www.gatherproxy.com/ru/sockslist'
     lis = [ (i*7)+2 for i  in  range(0,24)]
-    try:
-     r = requests.get(url)
+    soup = request(url)
 
-     soup = BeautifulSoup(r.text, 'html.parser')
+    td = soup.find('table', id='tblproxy').find_all('td')
 
-     td = soup.find('table', id='tblproxy').find_all('td')
-
-     ip_list = []
-     for  i in lis:
+    ip_list = []
+    for  i in lis:
 
           tds = td[i:i+2]
           ip= str(tds[0].find_all('script'))
@@ -50,21 +30,14 @@ def  pars ():
           ip=ip[53:67].replace("')",'').replace("'",'').replace('<',"")
           port = port[53:60].replace("')","").replace("<","")
           ip_list.append (f"soks5://{ip}:{port}")
-     return ip_list
-    except:
-        pass
+    return check_proxy(ip_list)
 
 
 def pars2():
     try:
         url = 'https://www.socks-proxy.net/'
         ip_list = []
-        try:
-            r = requests.get(url)
-            soup = BeautifulSoup(r.text, 'html.parser')
-
-        except:
-            return 'error1'
+        soup = request(url)
         ip = soup.find_all('td')
 
         for i in [5, 192]:
@@ -72,7 +45,28 @@ def pars2():
             port = str(ip)[i + 24:i + 30].replace("<", "").replace("/", "").replace('t', '').replace(">", "").replace("d","")
             ip_list.append(f"socks4://{ips}:{port}")
 
-        return ip_list
+        return check_proxy( ip_list)
 
     except:
        return  ' error'
+
+def request(url):
+  try:
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    return  soup
+  except:
+      pass
+
+def check_proxy(list):
+    for proxy in list:
+      a = {'http': proxy}
+      try:
+
+        requests.get("https://www.google.ru", proxies=a)
+
+        return proxy
+        break
+      except:
+        continue
+
